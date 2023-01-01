7dfe6606380415acdb3a368e9a8723e89a5e9208
임형묵 <skygudanr@dailybutton.co.kr>
2023-01-02 01:13:50 +0900

    전략 패턴

---
+++ src/main/java/strategy/_02_after/Client.java
@@ -0,0 +1,10 @@
+package strategy._02_after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        BlueLightRedLight game = new BlueLightRedLight();
+        game.blueLight(new Normal());
+        game.redLight(new Faster());
+    }
+}
---
+++ src/main/java/strategy/_02_after/Speed.java
@@ -0,0 +1,9 @@
+package strategy._02_after;
+
+public interface Speed {
+
+    void blueLight();
+
+    void redLight();
+
+}
