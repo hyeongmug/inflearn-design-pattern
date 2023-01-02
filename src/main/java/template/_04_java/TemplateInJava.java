f3877936ee4604f28c438656b8127dac4838edf8
임형묵 <skygudanr@dailybutton.co.kr>
2023-01-02 14:59:02 +0900

    자바에서 사용되는 템플릿 메소드 패턴

---
+++ src/main/java/template/_04_java/TemplateInJava.java
@@ -0,0 +1,10 @@
+package template._04_java;
+
+import javax.servlet.http.HttpServlet;
+
+public class TemplateInJava {
+
+    public static void main(String[] args) {
+        HttpServlet httpServlet = new MyHello();
+    }
+}
