3c31ca4f195e002d92d6a9bc169c9f516c1ab69a
임형묵 <skygudanr@dailybutton.co.kr>
2023-01-09 14:51:21 +0900

    자바에서 사용되는 팩토리 메소드 패턴

---
+++ src/main/java/factory_method/_03_java/SpringBeanFactoryExample.java
@@ -0,0 +1,18 @@
+package factory_method._03_java;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringBeanFactoryExample {
+
+    public static void main(String[] args) {
+        BeanFactory xmlFactory = new ClassPathXmlApplicationContext("config.xml");
+        String hello = xmlFactory.getBean("hello", String.class);
+        System.out.println(hello);
+
+        BeanFactory javaFactory = new AnnotationConfigApplicationContext(Config.class);
+        String hello1 = javaFactory.getBean("hello", String.class);
+        System.out.println(hello1);
+    }
+}
